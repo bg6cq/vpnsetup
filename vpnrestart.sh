4ee1c4f93e050eb2e4e079ca13c70d498579b0a6
Zhang Huanjie <james@ustc.edu.cn>
2020-04-02 08:27:26 +0800

    1.1-20200402

--- vpnrestart.sh
+++ vpnrestart.sh
@@ -47,6 +47,10 @@ if [ -z $SLAVE ]; then
 	SLAVE="NONE"
 fi
 
+if [ $MASTER == $SLAVE ]; then
+	SLAVE="NONE"
+fi
+
 if [ -z $IPV6 ]; then
 	IPV6="NO"
 fi
@@ -57,21 +61,21 @@ if [ $IPV6 == "YES" ]; then
 fi
 
 if [ $SLAVE = "NONE" ] ; then
-	/usr/src/ethudp/EthUDP -e $OPT $IP $PORT $REMOTE $PORT eth1
-	/usr/src/ethudp/EthUDP -i $OPT $IP $MPORT $REMOTE $MPORT $PREFIX$INDEX.2 24
+	/usr/src/ethudp/EthUDP -n NET -e $OPT $IP $PORT $REMOTE $PORT eth1
+	/usr/src/ethudp/EthUDP -n MGT -i $OPT $IP $MPORT $REMOTE $MPORT $PREFIX$INDEX.2 24
 	if [ $IPV6 = "YES" ]; then
 		PORT=`expr $PORT + 500`
-		/usr/src/ethudp/EthUDP -e $OPT $IP $PORT $REMOTE $PORT eth2
+		/usr/src/ethudp/EthUDP -n IPV6 -e $OPT $IP $PORT $REMOTE $PORT eth2
 	fi
 else
 	REMOTE2=$(cat /etc/ethudp/SITE/$SLAVE)
 	PORT2=`expr $PORT + 1000`
 	MPORT2=`expr $PORT2 + 100`
-	/usr/src/ethudp/EthUDP -e $OPT $IP $PORT $REMOTE $PORT eth1 $IP $PORT2 $REMOTE2 $PORT
-	/usr/src/ethudp/EthUDP -i $OPT $IP $MPORT $REMOTE $MPORT $PREFIX$INDEX.2 24 $IP $MPORT2 $REMOTE2 $MPORT
+	/usr/src/ethudp/EthUDP -n NET -e $OPT $IP $PORT $REMOTE $PORT eth1 $IP $PORT2 $REMOTE2 $PORT
+	/usr/src/ethudp/EthUDP -n MGT -i $OPT $IP $MPORT $REMOTE $MPORT $PREFIX$INDEX.2 24 $IP $MPORT2 $REMOTE2 $MPORT
 	if [ $IPV6 = "YES" ]; then
 		PORT=`expr $PORT + 500`
 		PORT2=`expr $PORT + 1000`
-		/usr/src/ethudp/EthUDP -e $OPT $IP $PORT $REMOTE $PORT eth2 $IP $PORT2 $REMOTE2 $PORT
+		/usr/src/ethudp/EthUDP -n IPV6 -e $OPT $IP $PORT $REMOTE $PORT eth2 $IP $PORT2 $REMOTE2 $PORT
 	fi
 fi
